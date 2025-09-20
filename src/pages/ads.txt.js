3a6a01cb07a6b363b7363a9a5ae2e8197d93a107
Muhammad Fiaz <contact@muhammadfiaz.com>
2025-09-20 08:11:36 +0530

    added ads

---
+++ src/pages/ads.txt.js
@@ -0,0 +1,12 @@
+import { GOOGLE_CONFIG } from "@/utils/google-config";
+
+export async function GET() {
+  const adsTxtContent = `google.com, ${GOOGLE_CONFIG.ADSENSE_CLIENT_ID.replace('ca-pub-', '')}, DIRECT, f08c47fec0942fa0
+`;
+
+  return new Response(adsTxtContent, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
