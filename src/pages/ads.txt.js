866e64dc069635d2d29d4c08a889696424e054d2
Muhammad Fiaz <contact@muhammadfiaz.com>
2025-09-21 13:02:54 +0530

    fixed ads issue

--- src/pages/ads.txt.js
+++ src/pages/ads.txt.js
@@ -1,7 +1,7 @@
 import { GOOGLE_CONFIG } from "@/utils/google-config";
 
 export async function GET() {
-  const adsTxtContent = `google.com, ${GOOGLE_CONFIG.ADSENSE_CLIENT_ID.replace('ca-pub-', '')}, DIRECT, f08c47fec0942fa0
+  const adsTxtContent = `google.com, pub-${GOOGLE_CONFIG.ADSENSE_CLIENT_ID.replace('ca-pub-', '')}, DIRECT, f08c47fec0942fa0
 `;
 
   return new Response(adsTxtContent, {
